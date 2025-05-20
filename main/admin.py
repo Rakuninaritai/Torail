070aa7d41161cf598294448d9ba0642303ce44e5
shuto <yamamotshuto0505@gmail.com>
2025-05-20 15:06:36 +0900

    adminでuser登録用

--- main/admin.py
+++ main/admin.py
@@ -6,3 +6,10 @@ admin.site.register(Subject)
 admin.site.register(Task)
 admin.site.register(Record)
 admin.site.register(Language)
+
+# adminでユーザー登録用
+from django.contrib.auth.admin import UserAdmin
+# 既存登録をいったん解除
+admin.site.unregister(User)
+# 公式の UserAdmin を再登録
+admin.site.register(User, UserAdmin)
